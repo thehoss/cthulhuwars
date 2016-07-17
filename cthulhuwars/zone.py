9cdc3971216aa5d2d7495b8aa966d679a7f44290
Jay Reynolds <masterblasterofdisaster@gmail.com>
2016-07-16 22:59:07 -0700

    fixed method name style

--- cthulhuwars/zone.py
+++ cthulhuwars/zone.py
@@ -19,31 +19,27 @@ class Zone:
     def __init__(self, name = 'unnamed', isOcean = False):
         self.name = name
         self.is_ocean = isOcean
-        self.desecration_state = False
         self.gate_state = GateState.noGate
         self.gate_unit  = None
         self.occupancy_list = []
 
-    def setGateState(self, gateState):
+    def set_gate_state(self, gateState):
         self.gate_state = gateState
 
-    def setGateUnit(self, unit):
+    def set_gate_unit(self, unit):
         self.gate_unit = unit
 
-    def addUnit(self, unit):
+    def add_unit(self, unit):
         self.occupancy_list.append(unit)
 
-    def removeUnit(self, unit):
+    def remove_unit(self, unit):
         try:
             index = self.occupancy_list.index(unit)
             self.occupancy_list.pop(index)
         except ValueError:
             pass
 
-    def setDesecrationState(self, desecrationState):
-        self.desecration_state = desecrationState
-
-    def getZoneState(self):
-        zoneState = (self.gate_state, self.gate_unit, self.occupancy_list, self.desecration_state)
+    def get_zone_state(self):
+        zoneState = (self.gate_state, self.gate_unit, self.occupancy_list)
         return zoneState
 
