f9b6ba68b327bebef8495760c2bfc642901c644f
DEREK HAASE <derekhaase@Derek-Haase.local>
2016-07-16 23:29:47 -0700

    use __ for name mangling

--- cthulhuwars/unit.py
+++ cthulhuwars/unit.py
@@ -26,44 +26,43 @@ class UnitState(Enum):
 
 
 class Unit:
-    def __init__(self, faction=Faction.cthulhu, unit_type=UnitType.cultist, combat_power=0, cost=1, base_movement=1,
-                 unit_state=UnitState.in_reserve):
-        self.faction = faction
-        self.unit_type = unit_type
-        self.combat_power = combat_power
-        self.cost = cost
-        self.base_movement = base_movement
-        self.unit_state = unit_state
+    def __init__(self, faction=Faction.cthulhu, unit_type=UnitType.cultist, combat_power=0, cost=1,  base_movement=1,  unit_state=UnitState.in_reserve):
+        self.__faction = faction
+        self.__unit_type = unit_type
+        self.__combat_power = combat_power
+        self.__cost = cost
+        self.__base_movement = base_movement
+        self.__unit_state = unit_state
 
     def get_faction(self):
-        return self.faction
+        return self.__faction
 
     def get_unit_type(self):
-        return self.unit_type
+        return self.__unit_type
 
     def set_unit_type(self, unit_type):
-        self.unit_type = unit_type
+        self.__unit_type = unit_type
 
     def get_combat_power(self):
-        return self.combat_power
+        return self.__combat_power
 
     def get_cost(self):
-        return self.cost
+        return self.__cost
 
     def set_cost(self, cost):
-        self.cost = cost
+        self.__cost = cost
 
     def get_base_movement(self):
-        return self.base_movement
+        return self.__base_movement
 
     def set_base_movement(self, base_movement):
-        self.base_movement = base_movement
+        self.__base_movement = base_movement
 
     def get_unit_state(self):
-        return self.unit_state
+        return self.__unit_state
 
     def set_unit_state(self, unit_state):
-        self.unit_state = unit_state
+        self.__unit_state = unit_state
 
 
 
