cfa67e9104b151719c36794a9c89314cdcd1f5ed
Jay Reynolds <masterblasterofdisaster@gmail.com>
2016-07-18 21:37:44 -0700

    removing name mangling for privacy since we want the subclass to have access to member variables.

--- cthulhuwars/unit.py
+++ cthulhuwars/unit.py
@@ -1,12 +1,5 @@
 from enum import Enum
 
-class UnitType(Enum):
-    cultist = 0
-    monsterA = 1
-    monsterB = 2
-    monsterC = 3
-    GOO = 4
-
 class Faction(Enum):
     cthulhu = 0
     black_goat = 1
@@ -26,43 +19,55 @@ class UnitState(Enum):
 
 
 class Unit:
-    def __init__(self, faction=Faction.cthulhu, unit_type=UnitType.cultist, combat_power=0, cost=1,  base_movement=1,  unit_state=UnitState.in_reserve):
-        self.__faction = faction
-        self.__unit_type = unit_type
-        self.__combat_power = combat_power
-        self.__cost = cost
-        self.__base_movement = base_movement
-        self.__unit_state = unit_state
+    def __init__(self, faction=Faction.cthulhu, combat_power=0, cost=1,  base_movement=1,
+                 unit_state=UnitState.in_reserve):
+        self._faction = faction
+        self._combat_power = combat_power
+        self._cost = cost
+        self._base_movement = base_movement
+        self._unit_state = unit_state
 
     def get_faction(self):
-        return self.__faction
-
-    def get_unit_type(self):
-        return self.__unit_type
-
-    def set_unit_type(self, unit_type):
-        self.__unit_type = unit_type
+        return self._faction
 
     def get_combat_power(self):
-        return self.__combat_power
+        return self._combat_power
 
     def get_cost(self):
-        return self.__cost
+        return self._cost
 
     def set_cost(self, cost):
-        self.__cost = cost
+        self._cost = cost
 
     def get_base_movement(self):
-        return self.__base_movement
+        return self._base_movement
 
     def set_base_movement(self, base_movement):
-        self.__base_movement = base_movement
+        self._base_movement = base_movement
 
     def get_unit_state(self):
-        return self.__unit_state
+        return self._unit_state
 
     def set_unit_state(self, unit_state):
-        self.__unit_state = unit_state
+        self._unit_state = unit_state
+
+
+class Cultist(Unit):
+    def __init__(self):
+        pass
+
+class Monster(Unit):
+    def __init__(self):
+        pass
+
+class GreatOldOne(Unit):
+    def __init__(self):
+        pass
+
+
+
+
+
 
 
 
